b08b8ab8802f5d5fb8c70c133d274b859fb072b4
Demonil 666 <demonil666@bk.ru>
2025-02-06 23:46:39 +0300

    update3

--- LogInForm.Designer.cs
+++ LogInForm.Designer.cs
@@ -33,81 +33,171 @@
             InButton = new Button();
             loginLabel = new Label();
             passLabel = new Label();
+            button2 = new Button();
+            panel1 = new Panel();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
             label1 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // loginBox
             // 
-            loginBox.Location = new Point(141, 71);
+            loginBox.Anchor = AnchorStyles.None;
+            loginBox.BackColor = Color.White;
+            loginBox.BorderStyle = BorderStyle.FixedSingle;
+            loginBox.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            loginBox.Location = new Point(131, 10);
             loginBox.Name = "loginBox";
-            loginBox.Size = new Size(258, 23);
+            loginBox.Size = new Size(223, 21);
             loginBox.TabIndex = 18;
             // 
             // passBox
             // 
-            passBox.Location = new Point(141, 100);
+            passBox.Anchor = AnchorStyles.None;
+            passBox.BackColor = Color.White;
+            passBox.BorderStyle = BorderStyle.FixedSingle;
+            passBox.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            passBox.Location = new Point(131, 53);
             passBox.Name = "passBox";
             passBox.PasswordChar = '*';
-            passBox.Size = new Size(258, 23);
+            passBox.Size = new Size(223, 21);
             passBox.TabIndex = 19;
             // 
             // InButton
             // 
-            InButton.Location = new Point(141, 129);
+            InButton.Anchor = AnchorStyles.None;
+            InButton.BackColor = Color.FromArgb(192, 192, 255);
+            InButton.FlatAppearance.BorderSize = 0;
+            InButton.FlatStyle = FlatStyle.Flat;
+            InButton.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            InButton.Location = new Point(0, 95);
             InButton.Name = "InButton";
-            InButton.Size = new Size(258, 23);
+            InButton.Size = new Size(354, 40);
             InButton.TabIndex = 20;
-            InButton.Text = "Регистрация / авторизация";
-            InButton.UseVisualStyleBackColor = true;
+            InButton.Text = "Авторизация";
+            InButton.UseVisualStyleBackColor = false;
             InButton.Click += InButton_Click;
             // 
             // loginLabel
             // 
-            loginLabel.AutoSize = true;
-            loginLabel.Location = new Point(58, 74);
+            loginLabel.Anchor = AnchorStyles.None;
+            loginLabel.BackColor = Color.FromArgb(192, 192, 255);
+            loginLabel.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            loginLabel.Location = new Point(0, 10);
             loginLabel.Name = "loginLabel";
-            loginLabel.Size = new Size(77, 15);
+            loginLabel.Size = new Size(125, 21);
             loginLabel.TabIndex = 21;
-            loginLabel.Text = "логин/почта";
+            loginLabel.Text = "ЛОГИН / ПОЧТА";
+            loginLabel.TextAlign = ContentAlignment.MiddleRight;
             // 
             // passLabel
             // 
-            passLabel.AutoSize = true;
-            passLabel.Location = new Point(88, 103);
+            passLabel.Anchor = AnchorStyles.None;
+            passLabel.BackColor = Color.FromArgb(192, 192, 255);
+            passLabel.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            passLabel.Location = new Point(0, 53);
             passLabel.Name = "passLabel";
-            passLabel.Size = new Size(47, 15);
+            passLabel.Size = new Size(125, 21);
             passLabel.TabIndex = 22;
-            passLabel.Text = "пароль";
+            passLabel.Text = "ПАРОЛЬ";
+            passLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.None;
+            button2.BackColor = Color.FromArgb(192, 192, 255);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button2.Location = new Point(0, 155);
+            button2.Name = "button2";
+            button2.Size = new Size(354, 24);
+            button2.TabIndex = 28;
+            button2.Text = "Зарегистрироваться";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.None;
+            panel1.BackColor = Color.FromArgb(192, 255, 192);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(loginBox);
+            panel1.Controls.Add(passLabel);
+            panel1.Controls.Add(passBox);
+            panel1.Controls.Add(loginLabel);
+            panel1.Controls.Add(InButton);
+            panel1.Location = new Point(15, 17);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(354, 202);
+            panel1.TabIndex = 29;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.None;
+            label4.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.Location = new Point(0, 182);
+            label4.Name = "label4";
+            label4.Size = new Size(354, 15);
+            label4.TabIndex = 32;
+            label4.Text = "Для создания учётной записи!";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.None;
+            label3.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(0, 137);
+            label3.Name = "label3";
+            label3.Size = new Size(354, 15);
+            label3.TabIndex = 31;
+            label3.Text = "Для входа в учётную запись!";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(0, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(354, 15);
+            label2.TabIndex = 30;
+            label2.Text = "Без логина мы не сможем понять, что Вы это Вы!";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(244, 155);
+            label1.Anchor = AnchorStyles.None;
+            label1.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(0, 77);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 26;
-            label1.Text = "label1";
-            label1.Click += label1_Click;
+            label1.Size = new Size(354, 15);
+            label1.TabIndex = 29;
+            label1.Text = "Пароль требуется для того, чтобы обезопасить ваши данные!";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // LogInForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            ClientSize = new Size(484, 461);
-            Controls.Add(label1);
-            Controls.Add(passLabel);
-            Controls.Add(loginLabel);
-            Controls.Add(InButton);
-            Controls.Add(passBox);
-            Controls.Add(loginBox);
+            BackColor = Color.FromArgb(192, 192, 255);
+            ClientSize = new Size(384, 236);
+            Controls.Add(panel1);
             MaximizeBox = false;
             Name = "LogInForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Questionnaire";
             Load += StartForm_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -116,6 +206,11 @@
         private Button InButton;
         private Label loginLabel;
         private Label passLabel;
+        private Button button2;
+        private Panel panel1;
         private Label label1;
+        private Label label2;
+        private Label label4;
+        private Label label3;
     }
 }
--- LogInForm.cs
+++ LogInForm.cs
@@ -25,88 +25,88 @@ namespace Questionnaire
         // сделал всё в лоадере, чтобы не добавлять лишнюю кнопку, лень :P
         private void StartForm_Load(object sender, EventArgs e)
         {
-            using (Context context = new())
-            {
-                context.Users.RemoveRange(context.Users);
-                context.Roles.RemoveRange(context.Roles);
-                context.Surveies.RemoveRange(context.Surveies);
-                context.Questions.RemoveRange(context.Questions);
-                context.Answers.RemoveRange(context.Answers);
-                context.AnswerSurveies.RemoveRange(context.AnswerSurveies);
-                context.AnswerQuestions.RemoveRange(context.AnswerQuestions);
-                context.SaveChanges();
-            }
-            using (Context context = new())
-            {
-                Role adminRole = new() { Name = "Admin" };
-                Role userRole = new() { Name = "User" };
-                context.Roles.AddRange(adminRole, userRole);
-                context.SaveChanges();
+            //using (Context context = new())
+            //{
+            //    context.Users.RemoveRange(context.Users);
+            //    context.Roles.RemoveRange(context.Roles);
+            //    context.Surveies.RemoveRange(context.Surveies);
+            //    context.Questions.RemoveRange(context.Questions);
+            //    context.Answers.RemoveRange(context.Answers);
+            //    context.AnswerSurveies.RemoveRange(context.AnswerSurveies);
+            //    context.AnswerQuestions.RemoveRange(context.AnswerQuestions);
+            //    context.SaveChanges();
+            //}
+            //using (Context context = new())
+            //{
+            //    Role adminRole = new() { Name = "Admin" };
+            //    Role userRole = new() { Name = "User" };
+            //    context.Roles.AddRange(adminRole, userRole);
+            //    context.SaveChanges();
 
-                User adminUser = new() { Login = "Admin", Email = "admin@gmail.com", Password = GetMD5Hash("Admin"), Role = adminRole };
-                User user1 = new() { Login = "12341234", Email = "user@gmail.com", Password = GetMD5Hash("1234"), Role = userRole };
-                context.Users.AddRange(adminUser, user1);
-                context.SaveChanges();
+            //    User adminUser = new() { Login = "Admin", Email = "admin@gmail.com", Password = GetMD5Hash("Admin"), Role = adminRole };
+            //    User user1 = new() { Login = "12341234", Email = "user@gmail.com", Password = GetMD5Hash("1234"), Role = userRole };
+            //    context.Users.AddRange(adminUser, user1);
+            //    context.SaveChanges();
 
-                Survey survey1 = new() { Name = "quest 1", User = user1 };
-                context.Surveies.AddRange(survey1);
-                context.SaveChanges();
+            //    Survey survey1 = new() { Name = "quest 1", User = user1 };
+            //    context.Surveies.AddRange(survey1);
+            //    context.SaveChanges();
 
-                Question question1_1 = new() { Name = "1 == 1", Survey = survey1 };
-                context.Questions.AddRange(question1_1);
-                context.SaveChanges();
+            //    Question question1_1 = new() { Name = "1 == 1", Survey = survey1 };
+            //    context.Questions.AddRange(question1_1);
+            //    context.SaveChanges();
 
-                Answer answer1_1_1 = new() { Name = "T", IsTrue = true, Question = question1_1 };
-                Answer answer1_1_2 = new() { Name = "F", Question = question1_1 };
-                Answer answer1_1_3 = new() { Name = "NS", Question = question1_1 };
-                context.Answers.AddRange(answer1_1_1, answer1_1_2, answer1_1_3);
-                context.SaveChanges();
+            //    Answer answer1_1_1 = new() { Name = "T", IsTrue = true, Question = question1_1 };
+            //    Answer answer1_1_2 = new() { Name = "F", Question = question1_1 };
+            //    Answer answer1_1_3 = new() { Name = "NS", Question = question1_1 };
+            //    context.Answers.AddRange(answer1_1_1, answer1_1_2, answer1_1_3);
+            //    context.SaveChanges();
 
-                Question question1_2 = new() { Name = "1 == 2", Survey = survey1 };
-                context.Questions.AddRange(question1_2);
-                context.SaveChanges();
+            //    Question question1_2 = new() { Name = "1 == 2", Survey = survey1 };
+            //    context.Questions.AddRange(question1_2);
+            //    context.SaveChanges();
 
-                Answer answer1_2_1 = new() { Name = "T", Question = question1_2 };
-                Answer answer1_2_2 = new() { Name = "F", IsTrue = true, Question = question1_2 };
-                Answer answer1_2_3 = new() { Name = "NS", Question = question1_2 };
-                context.Answers.AddRange(answer1_2_1, answer1_2_2, answer1_2_3);
-                context.SaveChanges();
+            //    Answer answer1_2_1 = new() { Name = "T", Question = question1_2 };
+            //    Answer answer1_2_2 = new() { Name = "F", IsTrue = true, Question = question1_2 };
+            //    Answer answer1_2_3 = new() { Name = "NS", Question = question1_2 };
+            //    context.Answers.AddRange(answer1_2_1, answer1_2_2, answer1_2_3);
+            //    context.SaveChanges();
 
-                Question question1_3 = new() { Name = "a == b", Survey = survey1 };
-                context.Questions.AddRange(question1_3);
-                context.SaveChanges();
+            //    Question question1_3 = new() { Name = "a == b", Survey = survey1 };
+            //    context.Questions.AddRange(question1_3);
+            //    context.SaveChanges();
 
-                Answer answer1_3_1 = new() { Name = "T", Question = question1_3 };
-                Answer answer1_3_2 = new() { Name = "F", Question = question1_3 };
-                Answer answer1_3_3 = new() { Name = "NS", IsTrue = true, Question = question1_3 };
-                context.Answers.AddRange(answer1_3_1, answer1_3_2, answer1_3_3);
-                context.SaveChanges();
-            }
-            using (Context context = new())
-            {
-                User user2 = new() { Login = "78907890", Email = "1234@gmail.com", Password = GetMD5Hash("7890"), Role = context.Roles.Where(role => role.Name == "User").First() };
-                context.Users.AddRange(user2);
+            //    Answer answer1_3_1 = new() { Name = "T", Question = question1_3 };
+            //    Answer answer1_3_2 = new() { Name = "F", Question = question1_3 };
+            //    Answer answer1_3_3 = new() { Name = "NS", IsTrue = true, Question = question1_3 };
+            //    context.Answers.AddRange(answer1_3_1, answer1_3_2, answer1_3_3);
+            //    context.SaveChanges();
+            //}
+            //using (Context context = new())
+            //{
+            //    User user2 = new() { Login = "78907890", Email = "1234@gmail.com", Password = GetMD5Hash("7890"), Role = context.Roles.Where(role => role.Name == "User").First() };
+            //    context.Users.AddRange(user2);
 
-                string thisIdSurvay = "quest 1";
-                Survey survayAnswer = context.Surveies.Where(survay => survay.Name == thisIdSurvay).First();
-                List<Question> questions = context.Questions.Where(quest => quest.Survey.Id == survayAnswer.Id).ToList();
-                List<string> usersAnswers = new() { "T", "T", "NS" };
-                List<Answer> answers = new();
-                for (int i = 0; i < questions.Count; i++)
-                {
-                    answers.Add(context.Answers.Where(answer => answer.Name == usersAnswers[i] && answer.Question.Id == questions[i].Id).First());
-                }
+            //    string thisIdSurvay = "quest 1";
+            //    Survey survayAnswer = context.Surveies.Where(survay => survay.Name == thisIdSurvay).First();
+            //    List<Question> questions = context.Questions.Where(quest => quest.Survey.Id == survayAnswer.Id).ToList();
+            //    List<string> usersAnswers = new() { "T", "T", "NS" };
+            //    List<Answer> answers = new();
+            //    for (int i = 0; i < questions.Count; i++)
+            //    {
+            //        answers.Add(context.Answers.Where(answer => answer.Name == usersAnswers[i] && answer.Question.Id == questions[i].Id).First());
+            //    }
 
-                AnswerSurvey answerSurvey = new() { Survey = survayAnswer, User = user2 };
-                context.AnswerSurveies.AddRange(answerSurvey);
+            //    AnswerSurvey answerSurvey = new() { Survey = survayAnswer, User = user2 };
+            //    context.AnswerSurveies.AddRange(answerSurvey);
 
-                List<AnswerQuestion> answerQuestions = [];
-                for (int i = 0; i < questions.Count && i < answers.Count; i++)
-                {
-                    context.AnswerQuestions.Add(new() { Question = questions[i], Answer = answers[i], AnswerSurvey = answerSurvey });
-                }
-                context.SaveChanges();
-            }
+            //    List<AnswerQuestion> answerQuestions = [];
+            //    for (int i = 0; i < questions.Count && i < answers.Count; i++)
+            //    {
+            //        context.AnswerQuestions.Add(new() { Question = questions[i], Answer = answers[i], AnswerSurvey = answerSurvey });
+            //    }
+            //    context.SaveChanges();
+            //}
         }
         // експериментировал с фреймами, типо включение других фреймов, чтобы было)))
 
@@ -161,7 +161,7 @@ namespace Questionnaire
             }
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
             LogOnForm form = new();
             Visible = false;
